e9df5540c74b37ea7563585ccd12ac61a67e56f4
vyklimavicius <vyklimavicius@gmail.com>
2020-05-05 14:41:18 -0500

    added GameI/GameImpl

--- core/src/main/java/com/klimavicius/Game.java
+++ core/src/main/java/com/klimavicius/Game.java
@@ -4,13 +4,13 @@ public interface Game {
 
     int getNumber();
     int getGuess();
-    void setGuess();
+    void setGuess(int guess);
     int getSmallest();
     int getBiggest();
     int getRemainGuesses();
     void reset();
     void check();
-    boolean isValidNumber();
+    boolean isValidNumberRange();
     boolean isGameWon();
     boolean isGameLost();
 
--- core/src/main/java/com/klimavicius/GameImpl.java
+++ core/src/main/java/com/klimavicius/GameImpl.java
@@ -11,60 +11,83 @@ public class GameImpl implements Game {
     private int number;
     private int guess;
     private int smallest;
-    private int largest;
+    private int biggest;
+    private int remainingGuesses;
+    private boolean validNumberRange = true;
 
     @Override
     public int getNumber() {
-        return 0;
+        return number;
     }
 
     @Override
     public int getGuess() {
-        return 0;
+        return guess;
     }
 
     @Override
-    public void setGuess() {
-
+    public void setGuess(int guess) {
+        this.guess = guess;
     }
 
     @Override
     public int getSmallest() {
-        return 0;
+        return smallest;
     }
 
     @Override
     public int getBiggest() {
-        return 0;
+        return biggest;
     }
 
     @Override
     public int getRemainGuesses() {
-        return 0;
+        return remainingGuesses;
     }
 
     @Override
     public void reset() {
-
+        smallest = 0;
+        guess = 0;
+        remainingGuesses = guessCount;
+        biggest = numberGenerator.getMaxNumber();
+        number = numberGenerator.next();
+        log.debug("The number is {}", number);
     }
 
     @Override
     public void check() {
+      checkValidNumberRange();
+      if(validNumberRange) {
+          if(guess > number) {
+              biggest = guess - 1;
+          }
 
+          if(guess < number) {
+              smallest = guess + 1;
+          }
+      }
+
+      remainingGuesses--;
     }
 
     @Override
-    public boolean isValidNumber() {
-        return false;
+    public boolean isValidNumberRange() {
+        return validNumberRange;
     }
 
     @Override
     public boolean isGameWon() {
-        return false;
+        return guess == number;
     }
 
     @Override
     public boolean isGameLost() {
-        return false;
+        return !isGameWon() && remainingGuesses <= 0;
+    }
+
+    //==Private method==
+    private void checkValidNumberRange() {
+        validNumberRange = (guess >= smallest) && (guess <= biggest);
     }
 }
